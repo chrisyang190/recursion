7b2863385dd76b2042e6b30b51ea4c3b2eb515c9
Chris Yang <chrisyang190@gmail.com>
2017-01-28 13:29:18 -0800

    Complete getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+ var body = document.body;
+ var res = [];
+
+ function walkDOM (node, criteria) {
+ 	//base
+ 	criteria(node);
+ 	node = node.firstChild;
+ 	//recursion
+ 	while(node){
+ 		walkDOM(node, criteria);
+ 		node = node.nextSibling;
+ 	}
+
+ }
+ //if element contains class, push the element;
+ function classCriteria(node){
+ 	if (node.classList && node.classList.contains(className)){
+ 		res.push(node);
+ 	}
+ }
+
+walkDOM(body,classCriteria);
+return res;
+
+
 };
