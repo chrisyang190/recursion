ba6d936b7226df3cbcfbcde3eeea14985e5c568a
Chris Yang <chrisyang190@gmail.com>
2017-01-28 12:01:20 -0800

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+//primitive: boolean, number, string
+  if (typeof obj == 'boolean' || typeof obj == 'number' || obj == null) {
+  	return ''+ obj +'';
+  } else if (typeof obj == 'string') {
+  	return "\"" + obj + "\"";
+  } 
+
+//functions and undefined
+  	else if (typeof obj == 'function' || obj == undefined){
+  		return "";
+  	}
+
+//array
+	else if (Array.isArray(obj)){
+		var arr = [];
+		obj.forEach(function(s){
+			arr.push(stringifyJSON(s));
+		});
+		return "["+arr+"]";
+	}
+
+//object
+	else if (typeof obj == 'object'){
+		var str = "";
+		var keyArray = Object.keys(obj);
+		for(key in obj){
+			if (typeof obj[key] == 'function' || typeof obj[key] == 'undefined'){
+				//str = str;
+			} else if (keyArray.length >1 && key !== keyArray[keyArray.length-1]){
+				str = str+stringifyJSON(key) + ":"+ stringifyJSON(obj[key])+",";
+			} else {
+				str = str+stringifyJSON(key) + ":"+ stringifyJSON(obj[key]);
+			}
+		}
+		return "{"+str+"}";
+	}
+ 
 };
